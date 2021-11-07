fa0fb49d97be17e79a2a3692bede149a8c04c120
Afolabi Abdulqadri <afolabizerotohero@gmail.com>
2021-11-08 02:26:28 +0530

    updated repo in corrected format

---
+++ Group5-Module1-Afolabi/Basic Python.py
@@ -0,0 +1,17 @@
+# importing the required libraries
+import shutil
+from keras.preprocessing.image import load_img,save_img,img_to_array
+
+
+def copy_and_resize(source_folder_with_file_name,destination_folder,target_image_size = (128,128)):
+    """
+    Use forward slashes for directoties
+    """
+
+    ### resizing the image
+    source_folder_with_file = string
+    img = load_img(source_folder_with_file_name, grayscale=False ,target_size=target_image_size)
+    img_array = img_to_array(img)
+    save_img(source_folder_with_file_name, img_array)
+    ### coping the image file 
+    shutil.copy2(source_folder_with_file_name, destination_folder)
---
+++ Group5-Module1-Afolabi/README.md
@@ -0,0 +1,5 @@
+# cil-cohort-1-
+CIL Cohort 1 - Class Repo
+
+Candidate ID - 004
+Name : Afolabi Abdulqadri Olaniyi
